9dbd1bfc6f34215449b8159b9b941e91bd2e68fd
Omri Gold <goldomri@gmail.com>
2023-05-30 18:34:13 +0300

    error handling

--- manga_reccomendation.py
+++ manga_reccomendation.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import ast
 
+NOT_FOUND_TITLE_MSG = "Didn't find manga title, please try again"
+
 # %% Load and preprocess data
 manga_data = pd.read_csv("manga.csv")
 
@@ -11,10 +13,18 @@ manga_list_features = ['genres', 'demographics', 'themes', 'serializations']
 for col in manga_list_features:
     manga_data[col] = manga_data[col].apply(ast.literal_eval)
 
+manga_dtype_Int64 = ['volumes', 'chapters']
+for col in manga_dtype_Int64:
+    manga_data[col].fillna(np.round((manga_data[col].mean())), inplace=True)
+    manga_data[col] = manga_data[col].astype('Int64')
+
+manga_data['score'].fillna(np.round((manga_data['score'].mean())), inplace=True)
+manga_data['score'] = manga_data['score'].astype('Float64')
+
 # Remove unnecessary columns
 manga_drop = ['start_date', 'end_date', 'created_at_before', 'updated_at', 'real_start_date', 'real_end_date',
               'background', 'main_picture', 'url', 'title_english', 'title_japanese', 'title_synonyms', 'authors',
-              'manga_id', 'scored_by', 'score', 'volumes', 'chapters', 'members', 'favorites']
+              'manga_id', 'scored_by', 'members']
 manga_data = manga_data.drop(manga_drop, axis=1)
 
 # Choose only approved manga
@@ -29,7 +39,7 @@ for col in manga_categorical_features:
     manga_data = pd.get_dummies(manga_data, columns=[col]).groupby(level=0).sum()
 
 for col in manga_list_features:
-    categorical_features = pd.get_dummies(manga_data[col].explode())
+    categorical_features = pd.get_dummies(manga_data[col].explode(), prefix=col)
     manga_data = pd.concat([manga_data, categorical_features.groupby(level=0).sum()], axis=1).drop(col, axis=1)
 
 del categorical_features
@@ -38,7 +48,7 @@ del categorical_features
 manga_features = manga_data.drop(['title', 'synopsis'], axis=1)
 
 num_of_neighbors = 6
-nbrs = NearestNeighbors(n_neighbors=num_of_neighbors, algorithm='ball_tree').fit(manga_features)
+nbrs = NearestNeighbors(n_neighbors=num_of_neighbors).fit(manga_features)
 distances, indices = nbrs.kneighbors(manga_features)
 
 
@@ -49,13 +59,18 @@ def get_index_from_title(title):
     :param title: The title of the wanted manga
     :return: The index of the manga in the dataset
     """
-    return manga_data[manga_data['title'].str.lower() == title.lower()].index.tolist()[0]
+    try:
+        return manga_data[manga_data['title'].str.casefold().str.replace('s/+', "") ==
+                          title.casefold().replace('s/+', "")].index.tolist()[0]
+    except IndexError:
+        print(NOT_FOUND_TITLE_MSG)
 
 
 def print_similar_manga(title, synopsis=False):
     """
     Prints 5 similar manga to a given title.
     :param title: The specific manga title
+    :param synopsis: If True, the program will print the synopsis of similar manga
     :return: None
     """
     found_id = get_index_from_title(title)
